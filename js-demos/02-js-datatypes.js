ae279c627db18adccf5191394c4083251a47c737
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-28 11:13:22 +0530

    Update 02-js-datatypes.js

--- js-demos/02-js-datatypes.js
+++ js-demos/02-js-datatypes.js
@@ -1,11 +1,32 @@
 
 // Datatypes in JavaScript 
 
-let num = 10;
+// Java        int num = 10;
+//             double salary;
+//                 num = "aaaaa";  X - CE 
+
+//              int num2 = "Vaman"; X - CE 
+
+// JavaScript  let num = 10;
+
+
+let num;
 console.log(num);
+console.log(typeof(num));
+num = 10; // number 
+console.log(num);
+console.log(typeof(num));
+num = "Capgemini"; // string 
+console.log(num);
+console.log(typeof(num));
+num = false; // boolean 
+console.log(num);
+console.log(typeof(num));
 
-let name = "Vaman";
-console.log(name);
 
-let isItThere = true;
-console.log(isItThere);
+
+// let name = "Vaman";
+// console.log(name);
+
+// let isItThere = true;
+// console.log(isItThere);
